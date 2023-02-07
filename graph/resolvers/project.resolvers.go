07984409fd70245e4ae9914b9c7c024aad7e6ee0
Luis Arce <luis.arce22@gmail.com>
2023-02-06 22:26:34 -0400

    Project creation and list resolvers

--- graph/resolvers/project.resolvers.go
+++ graph/resolvers/project.resolvers.go
@@ -2,16 +2,55 @@ package resolvers
 
 import (
 	"context"
-	"fmt"
+	"stats-mockupero/graph/common"
 	"stats-mockupero/graph/model"
+	models "stats-mockupero/graph/models"
+	"strconv"
 )
 
 // CreateProject is the resolver for the createProject field.
 func (r *mutationResolver) CreateProject(ctx context.Context, input model.NewProject) (*model.GqlProject, error) {
-	panic(fmt.Errorf("not implemented: CreateProject - createProject"))
+	project := &models.Project{
+		Name:        input.Name,
+		Description: *input.Description,
+	}
+
+	context := common.GetContext(ctx)
+	err := context.Database.Create(&project).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	projectResult := &model.GqlProject{
+		ID:          strconv.FormatUint(uint64(project.ID), 10),
+		Name:        project.Name,
+		Description: &project.Description,
+	}
+
+	return projectResult, nil
 }
 
 // Projects is the resolver for the projects field.
 func (r *queryResolver) Projects(ctx context.Context) ([]*model.GqlProject, error) {
-	panic(fmt.Errorf("not implemented: Projects - projects"))
+	context := common.GetContext(ctx)
+
+	var projects []*models.Project
+	var projectsResult []*model.GqlProject
+
+	err := context.Database.Find(&projects).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, project := range projects {
+		projectsResult = append(projectsResult, &model.GqlProject{
+			ID:          strconv.FormatUint(uint64(project.ID), 10),
+			Name:        project.Name,
+			Description: &project.Description,
+		})
+	}
+
+	return projectsResult, nil
 }
